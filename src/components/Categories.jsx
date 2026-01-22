fd401931cf8a9665fc1dd6efa63d603861f2b40c
zakv-v <zakvova@i.ua>
2026-01-22 19:08:50 +0200

    #2 create component, scss, import/export, props

---
+++ src/components/Categories.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+
+const Categories = (props) => {
+  return (
+    <div>
+      <div className="categories">
+        <ul>
+          <li className="active">Все</li>
+          <li>Мясные</li>
+          <li>Вегетарианская</li>
+          <li>Гриль</li>
+          <li>Острые</li>
+          <li>Закрытые</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+export default Categories;
